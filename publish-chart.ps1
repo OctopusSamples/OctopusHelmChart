6e924f72b5d8ca10f400095a28a8d089e31f87c4
Matthew Casperson <matthewcasperson@gmail.com>
2020-07-14 08:23:23 +1000

    Seriously! No wildcards in aws s3 cp???

--- publish-chart.ps1
+++ publish-chart.ps1
@@ -2,5 +2,5 @@ helm package .
 aws s3 cp s3://octopus-helm-charts/index.yaml .
 helm repo index . --url https://octopus-helm-charts.s3.amazonaws.com --merge index.yaml
 ls
-aws s3 cp *.tgz s3://octopus-helm-charts
+Get-ChildItem . -Filter *.tgz | %{aws s3 cp $_.Name s3://octopus-helm-charts}
 aws s3 cp index.yaml s3://octopus-helm-charts
