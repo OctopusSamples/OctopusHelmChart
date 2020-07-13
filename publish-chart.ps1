f28e01013db3cb9afa5c4920db95ccc17f0cc5b9
Matthew Casperson <matthewcasperson@gmail.com>
2020-07-14 08:17:15 +1000

    Debugging deployment

--- publish-chart.ps1
+++ publish-chart.ps1
@@ -1,5 +1,6 @@
 helm package .
 aws s3 cp s3://octopus-helm-charts/index.yaml .
 helm repo index . --url https://octopus-helm-charts.s3.amazonaws.com --merge index.yaml
+ls
 aws s3 cp *.tgz s3://octopus-helm-charts
 aws s3 cp index.yaml s3://octopus-helm-charts
